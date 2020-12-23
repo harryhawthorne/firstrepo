d3ff16143982e1ac25da2ab5141eb723f499468b
Harry <harryhawthorne.hh@gmail.com>
2020-12-23 13:01:15 +1100

    Added multifile

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
 print("Hello, test.py")
 print("Added second line to test.py")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+
+import test2
+
+test2.harry()
--- test2.py
+++ test2.py
@@ -1,3 +1,6 @@
 print("Hello, test2.py")
 print("Second line")
 print("Third line")
+
+def harry():
+    print("Hello harry")
